a4a8d4fe7a8f94d8360df0186a37787604a56079
Sufiyaan Usmani <sufiyaanusmani@ubuntu22.com>
2023-03-18 17:18:45 +0500

    add code to get regular users

---
+++ utility/regularusers.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+void storeInFile(string path, string str)
+{
+    ofstream outfile(path);
+    outfile << str;
+    outfile.close();
+}
+
+string filterOutput(string result)
+{
+    string output = "";
+    stringstream ss(result);
+    string line;
+    while (getline(ss, line))
+    {
+        size_t firstNonSpace = line.find_first_not_of(' ');
+        line = line.substr(firstNonSpace);
+        output = output + line + "\n";
+    }
+    return output;
+}
+
+int main()
+{
+    char buffer[1024];
+    string result = "";
+    string path;
+    string directoryName = "/tmp/kernelkit";
+
+    mkdir(directoryName.c_str(), 0777);
+    // Run the Linux command and open a pipe to read its output
+    FILE *pipe = popen("awk -F: '{if ($3 >= 1000 && $3 < 65534) print}' /etc/passwd | cut -d':' -f1,3,4,6", "r");
+    if (!pipe)
+    {
+        cerr << "Failed to run command" << std::endl;
+        return 1;
+    }
+
+    // Read the output of the command line by line
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL)
+    {
+        result += buffer;
+    }
+
+    // Close the pipe and print the output
+    pclose(pipe);
+    path = directoryName + "/regularusers";
+    result = filterOutput(result);
+    storeInFile(path, result);
+
+    return 0;
+}
