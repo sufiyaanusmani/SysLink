7d65a8c76c2f8df0152e10b5c81f73167079eee8
Sufiyaan Usmani <sufiyaanusmani@ubuntu22.com>
2023-03-18 17:04:44 +0500

    add code for scheduled shutdown and reboot

---
+++ utility/shutdown.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2 || argc == 3)
+    {
+        string pass = argv[1];
+        string command = "";
+        int time;
+        if (argc == 2)
+        {
+            command = "echo " + pass + " | sudo -S shutdown now";
+        }
+        else
+        {
+            time = stoi(argv[2]);
+            command = "echo " + pass + " | sudo -S shutdown +" + to_string(time);
+        }
+        system(command.c_str());
+    }
+    return 0;
+}
