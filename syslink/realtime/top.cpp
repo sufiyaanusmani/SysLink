0e0217d551d4c23b90757f477c66aaf727a4c733
Sufiyaan Usmani <sufiyaanusmani@ubuntu22.com>
2023-04-16 13:47:00 +0500

    add feature in top.cpp to allow user to enter reloading time, default is 2s

--- syslink/realtime/top.cpp
+++ syslink/realtime/top.cpp
@@ -6,8 +6,21 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    int time;
+    if (argc == 1)
+    {
+        time = 2;
+    }
+    else if (argc == 2)
+    {
+        time = stoi(argv[1]);
+    }
+    else
+    {
+        exit(1);
+    }
     string directoryName = "/tmp/syslink";
 
     mkdir(directoryName.c_str(), 0777);
@@ -15,7 +28,7 @@ int main()
     {
         string command = "top -b -n 1 | head -n 27 | tail -n 20 | awk '{print $1,$2,$9,$10,$11,$12}' | sed 's/ /@/g' > /tmp/syslink/top";
         system(command.c_str());
-        this_thread::sleep_for(chrono::seconds(2));
+        this_thread::sleep_for(chrono::seconds(time));
     }
     return 0;
 }
