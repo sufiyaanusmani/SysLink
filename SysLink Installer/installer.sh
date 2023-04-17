b85689aa17f3ad44ad734a31550f6f43600fdad1
Sufiyaan Usmani <sufiyaanusmani@ubuntu22.com>
2023-04-17 11:34:53 +0500

    add code to install speedtest-cli for speedtest

--- SysLink Installer/installer.sh
+++ SysLink Installer/installer.sh
@@ -1,4 +1,5 @@
 echo $1 | sudo -S rm -rf /syslink 2> /dev/null
+echo $1 | sudo -S apt install -y speedtest-cli
 g++ syslink-installer.cpp -o syslink-installer
 ./syslink-installer $1
 echo $1 | sudo -S chmod 777 -R /syslink
